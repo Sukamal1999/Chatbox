0d36c57b5381131405b4c0b933c333570e3e0d8f
Sukamal <sukamalmondal07@gmail.com>
2024-01-23 10:19:57 +0530

    implement of CORS

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sequelize = require("./util/database");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
