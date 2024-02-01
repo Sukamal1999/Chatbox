c2b1df38bd5772a00ab2b3bf301e3de880a1298a
Sukamal <sukamalmondal07@gmail.com>
2024-02-01 10:59:33 +0530

    Build the frontend of the chat Application

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
 
-const cors = require("cors");
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// const cors = require("cors");
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -20,3 +20,30 @@ const sequelize = require("./util/database");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const userRouter = require("./router/userRouter");
+const homePageRouter = require("./router/homePageRouter");
+const chatRouter = require("./router/chatRouter");
+const groupRouter = require("./router/groupRouter");
+
+
+const User = require("./models/userModel");
+const Chat = require("./models/chatModel");
+const Group = require("./models/groupModel");
+const UserGroup = require("./models/userGroup");
+
+app.use("/", userRouter);
+app.use("/user", userRouter);
+
+app.use("/homePage", homePageRouter);
+
+app.use("/chat", chatRouter);
+
+app.use("/group", groupRouter);
+
+sequelize
+  .sync({ force: true })
+  .then((result) => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => console.log(err));
